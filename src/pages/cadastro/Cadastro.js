f1210369c4b75165c34e05e1320959e3b4c93cdb
sarahrubia <sarahrubia22@gmail.com>
2022-08-03 19:16:29 -0300

    Cadastro page update, airtable integration working

--- src/pages/cadastro/Cadastro.js
+++ src/pages/cadastro/Cadastro.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./cadastro-styles";
 
 function Cadastro() {
+  const [productName, setProductName] = useState("");
+  const [repetition, setRepetition] = useState(0);
+  const [repetitionDay, setRepetitionDay] = useState(0);
+  const [endindDay, setEndingDay] = useState(1660521600);
+
+  const postData = (e) => {
+    e.preventDefault();
+    // Airtable Connection and Configuration
+    var Airtable = require("airtable");
+    var base = new Airtable({ apiKey: "keyIQRTXBdJyaVJaz" }).base(
+      "app4vUGC2nxXBaIY7"
+    );
+
+    // Creating a new record and posting on Airtable
+    base("Produtos").create(
+      {
+        id_usuario: "0277a69cf889d21e9614966db20e858a",
+        nome: productName,
+        repeticao: parseInt(repetition),
+        repeticao_dia: parseInt(repetitionDay),
+        encerramento: endindDay,
+        data_criacao: new Date().getTime() / 1000,
+      },
+      function (err, record) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(record.getId());
+      }
+    );
+  };
+
+  const handleDate = (e) => {
+    var getDate = e.target.value;
+    var SelectedDate = new Date(getDate).getTime() / 1000;
+
+    setEndingDay(SelectedDate);
+  };
+
   return (
     <S.Container>
       <h1>Cadastro de Produtos</h1>
 
       {/* Product registration form */}
-      <S.Form>
+      <S.Form onSubmit={postData}>
         {/* Insert title */}
-        <input type="text" placeholder="Adicionar nome do produto" />
+        <label htmlFor="product-name"></label>
+        <input
+          type="text"
+          name="product-name"
+          placeholder="Nome do produto"
+          onChange={(e) => setProductName(e.target.value)}
+        />
 
         {/* Insert how many times repetition will occur */}
         <div>
@@ -20,58 +66,47 @@ function Cadastro() {
             step="1"
             name="repetition"
             id="repetition"
+            onChange={(e) => setRepetition(e.target.value)}
           />
           {/* Select repetition step (day, week, month) */}
-          <select
-            name="repetition-step"
-            id="repetition-step"
-            placeholder="Não se repete"
-          >
-            <option value="repetition-step-week">semana</option>
-            <option value="repetition-step-month">mês</option>
-          </select>
+          <span>semana(s)</span>
         </div>
 
         {/* Select product recurrence by week day */}
         <div>
           <label htmlFor="week-repetition">Repetir:</label>
-          <S.FlexDiv>
-            <S.WeekButtons>D</S.WeekButtons>
-            <S.WeekButtons>S</S.WeekButtons>
-            <S.WeekButtons>T</S.WeekButtons>
-            <S.WeekButtons>Q</S.WeekButtons>
-            <S.WeekButtons>Q</S.WeekButtons>
-            <S.WeekButtons>S</S.WeekButtons>
-            <S.WeekButtons>S</S.WeekButtons>
-          </S.FlexDiv>
         </div>
+        <S.FlexDiv onChange={(e) => setRepetitionDay(e.target.value)}>
+          <S.RadioButton type="radio" name="weekday" id="sun" value="0" />
+          <label htmlFor="weekday">D</label>
+          <S.RadioButton type="radio" name="weekday" id="mon" value="1" />
+          <label htmlFor="">S</label>
+          <S.RadioButton type="radio" name="weekday" id="tue" value="2" />
+          <label htmlFor="">T</label>
+          <S.RadioButton type="radio" name="weekday" id="wed" value="3" />
+          <label htmlFor="">Q</label>
+          <S.RadioButton type="radio" name="weekday" id="thu" value="4" />
+          <label htmlFor="">Q</label>
+          <S.RadioButton type="radio" name="weekday" id="fri" value="5" />
+          <label htmlFor="">S</label>
+          <S.RadioButton type="radio" name="weekday" id="sat" value="6" />
+          <label htmlFor="">S</label>
+        </S.FlexDiv>
 
         {/* Select when repetition will end */}
         <S.FlexColumnDiv>
           <label>Termina em:</label>
           <div>
-            <input
-              type="radio"
-              name="repetition-ends"
-              value="never"
-              id="never"
-            />
+            <input type="radio" name="repetition-ends" id="never" />
             <label htmlFor="repetition-ends-never">Nunca</label>
           </div>
           <div>
-            <input type="radio" name="repetition-ends" value="date" id="date" />
-            <label htmlFor="repetition-ends-date">Em</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              name="repetition-ends"
-              value="after"
-              id="after"
-            />
-            <label htmlFor="repetition-ends-after">Após</label>
+            <input type="radio" name="repetition-ends" id="date" />
+            <label>Em: </label>
+            <input type="date" onChange={handleDate} />
           </div>
         </S.FlexColumnDiv>
+        <button type="submit">+ Cadastrar</button>
       </S.Form>
     </S.Container>
   );
--- src/pages/cadastro/cadastro-styles.js
+++ src/pages/cadastro/cadastro-styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
+// import { css } from "styled-components";
 
 export const Container = styled.div`
   width: 375px;
-  background-color: var(--withe);
+  background-color: var(--shape);
   color: var(--green_dark);
 `;
 
 export const Form = styled.form`
-  background-color: var(--shape);
   padding: 20px;
 `;
 
@@ -15,18 +15,16 @@ export const FlexDiv = styled.div`
   display: flex;
 `;
 
-export const WeekButtons = styled.a`
-  cursor: pointer;
-  display: inline-block;
-  border-radius: 50%;
-  width: 100%;
-  padding: 10px;
-  margin: 3px;
-  background: var(--green_dark);
-  color: var(--withe);
+export const RadioButton = styled.input`
+  color: var(--green_dark);
+  margin-left: 15px;
 `;
 
 export const FlexColumnDiv = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+export const Div = styled.div`
+  padding: 10px 0 15px 0;
 `;
