ee3a804ea38f5fe4d63dded2485eee99dc851a20
Victor Mota <victormotavctr@gmail.com>
2022-08-03 17:39:38 -0300

    Login page update, form and styles added

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,58 @@
-import * as S from './login-styles'
+import * as S from "./login-styles";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
 function Login() {
-    return (
-        <S.Container>
-            <h1>Login</h1>
-        </S.Container>
-    )
+  const handleClickLogin = (values) => {
+    console.log(values);
+  };
+  const validationLogin = yup.object().shape({
+    email: yup.string().email("Email inválido!").required("Campo obrigatório!"),
+    cpf: yup
+      .string()
+      .min(11, "CPF inválido!")
+      .max(11, "CPF inválido!")
+      .required("Campo obrigatório!"),
+  });
+
+  return (
+    <div>
+      <S.Image>
+        <h1>!!!!</h1>
+      </S.Image>
+
+      <S.Title>
+        <div></div>
+        <p>LOGIN/ SING UP</p>
+        <div></div>
+      </S.Title>
+
+      <S.Login>
+        <Formik
+          initialValues={{}}
+          onSubmit={handleClickLogin}
+          validationSchema={validationLogin}
+        >
+          <S.LoginForm>
+            <Form>
+              <S.Email>
+                <p>Email</p>
+                <Field component="input" name="email"></Field>
+                <ErrorMessage component="span" name="email"></ErrorMessage>
+              </S.Email>
+              <S.Cpf>
+                <p>CPF</p>
+                <Field name="cpf"></Field>
+                <ErrorMessage component="span" name="cpf"></ErrorMessage>
+              </S.Cpf>
+
+              <button type="submit">LOGIN</button>
+            </Form>
+          </S.LoginForm>
+        </Formik>
+      </S.Login>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/login/login-styles.js
+++ src/pages/login/login-styles.js
@@ -1,5 +1,88 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Container = styled.div`
-    background-color: var(--green_dark);
-`
+export const Image = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 15px;
+  font-size: 35px;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: 105px;
+    height: 1px;
+    background-color: black;
+    margin: 10px;
+    background-color: #00000033;
+  }
+  p {
+    color: #00000080;
+    font-weight: 600;
+  }
+`;
+
+export const Login = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 15px;
+  height: 250px;
+  width: 95%;
+`;
+
+export const LoginForm = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 95%;
+
+  input {
+    background: #ffff;
+    border: 1px solid #00000033;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    width: 320px;
+    padding: 15px;
+    font-size: 15px;
+  }
+  p {
+    font-size: 14px;
+    margin-bottom: 2px;
+  }
+
+  span {
+    color: red;
+    display: flex;
+    justify-content: center;
+    margin-top: 2px;
+    font-size: 14px;
+  }
+
+  button {
+    width: 100%;
+    padding: 15px;
+    background: #395b64;
+    color: #ffff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border: 1px solid #00000033;
+    border-radius: 40px;
+    font-size: 20px;
+    margin-top: 10px;
+    cursor: pointer;
+    transition: 200ms;
+  }
+
+  button:hover {
+    background-color: #295b64;
+  }
+`;
+export const Email = styled.div`
+  height: 95px;
+`;
+export const Cpf = styled.div`
+  height: 95px;
+`;
