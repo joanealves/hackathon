583fc8ca1cbbc8e5d31998732f9150151d3ca509
Victor Mota <victormotavctr@gmail.com>
2022-08-07 14:35:34 -0300

    listing page updates

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -1,7 +1,20 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 export function PrivateRoute({ children }) {
-  const user = true;
+  const [user, setUser] = useState(true);
+
+  //Fazer funcionar
+
+  useEffect(() => {
+    const hash = JSON.parse(localStorage.getItem("users"));
+    if (hash != "") {
+      console.log("VALOR HASH ATUAL", hash);
+      setUser(true);
+      console.log(user);
+    }
+  }, []);
 
   return user ? children : <Navigate to="/" />;
 }
