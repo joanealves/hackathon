181d4ff8f575cee789f959bfff0269977ec6cdcc
Joane Alves <joanealves2011@gmail.com>
2022-08-08 01:13:20 -0300

    conflict build dependencie useeffect

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function PrivateRoute({ children }) {
   const [user, setUser] = useState(false);
 
-  useEffect(() => {
-    const hash = JSON.parse(localStorage.getItem("users"));
-    if (hash !== "") {
-      console.log("VALOR HASH ATUAL", hash);
-      setUser(true);
-      console.log(user);
-    }
-  });
+  const hash = JSON.parse(localStorage.getItem("users"));
+  if (hash !== "") {
+    console.log("VALOR HASH ATUAL", hash);
+    setUser(true);
+  }
 
   return user ? children : <div></div>;
 }
